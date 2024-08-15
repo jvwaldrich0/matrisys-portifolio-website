2d1e565e1eed95847de99bc4834bc102ed2cce8d
KneibL1 <eliukneib@proton.me>
2024-08-15 16:50:38 -0300

    Animação Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -218,4 +218,24 @@
 
   });
 
+  /* Navmenu change */
+  document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.section');
+    const navLinks = document.querySelectorAll('#navmenu a');
+
+    function changeActiveLink() {
+        let index = sections.length;
+
+        while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        navLinks.forEach((link) => link.classList.remove('active'));
+        if (navLinks[index]) {
+            navLinks[index].classList.add('active');
+        }
+    }
+
+    changeActiveLink();
+    window.addEventListener('scroll', changeActiveLink);
+});
+
 })();
